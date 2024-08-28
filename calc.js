f0d9aabc8435c1906f1a143a4c60902d1728c509
Francis Simy Nazareth <73844173+francisnazareth@users.noreply.github.com>
2024-08-28 13:12:03 +0300

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,1 +1,11 @@
 
+window.onload = function() {
+    var canvas = document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
+
+    // Set the fill color to blue
+    ctx.fillStyle = '#0000FF';
+
+    // Draw a rectangle at coordinates (50, 50) with width 200 and height 100
+    ctx.fillRect(50, 50, 200, 100);
+};
